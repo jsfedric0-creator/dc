8d35a2a052a9c701bd53312cae649c7f12402af1
jsfedric0-creator <jsfedric0@gmail.com>
2026-01-17 17:21:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
-# start.sh
+#!/bin/bash
+
+# Create directories
+mkdir -p /app/data /tmp/hls /tmp/dash
+
+# Initialize database
+python3 -c "
+from dashboard.app import init_db
+init_db()
+print('Database initialized')
+"
+
+# Start services
+supervisord -c /etc/supervisor/conf.d/supervisord.conf
